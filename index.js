98826b8793e1f947e0d3552b8b73bda51f585707
Kaelinator <kirkkael@gmail.com>
2018-04-06 19:32:27 -0500

    Emailing is a go!

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 'use strict'
-console.log('Hi there!')
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+
+nodemailer.createTestAccount((err, account) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  })
+
+  const mailOptions = {
+    from: `"Kaelinator" <${process.env.GMAIL_USER}>`,
+    to: `${process.env.RECIPIENT}`,
+    subject: 'Test email',
+    text: 'Hi there',
+    html: '<b>Woah!</b>'
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error)
+      return 
+    }
+    console.log('Sent', info.messageId)
+  })
+
+
+})

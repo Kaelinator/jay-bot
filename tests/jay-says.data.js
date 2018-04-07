a2c77e33b8452aa20df97618d17aa1459523ff08
Kaelinator <kirkkael@gmail.com>
2018-04-07 11:31:48 -0500

    piecing data together

--- tests/jay-says.data.js
+++ tests/jay-says.data.js
@@ -1,8 +1,19 @@
 
 const positives = [
-  'jschulz needs 1 peep from each to return papers.'
+  'jschulz needs 1 peep from each to return papers.',
+  'If you wish to return papers, then respond to this email. jay will take the first 3 takers from each period.'
+]
+
+const negatives = [
+  'Arriaga , Moore (H), lopez, and schluter win!'
+]
+
+const falsePositives = [
+  'This exercise needs to be polished and returned BOP on Monday, April 9.'
 ]
 
 module.exports = {
-  positives
+  positives,
+  negatives,
+  falsePositives
 }
